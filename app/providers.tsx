7571f4a5fcade8234b85bbd1beabda900ba186cc
joyboyy1221 <ha4325578@gmail.com>
2026-07-26 04:17:45 +0500

    fix: ssr false and chain id hardcoded

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { RainbowKitProvider, getDefaultConfig } from "@rainbow-me/rainbowkit";
+import { RainbowKitProvider, getDefaultConfig, darkTheme } from "@rainbow-me/rainbowkit";
 import { WagmiProvider, http } from "wagmi";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import "@rainbow-me/rainbowkit/styles.css";
@@ -20,7 +20,7 @@ const config = getDefaultConfig({
   transports: {
     [arcTestnet.id]: http("https://rpc.testnet.arc.network"),
   },
-  ssr: true,
+  ssr: false,
 });
 
 const queryClient = new QueryClient();
@@ -29,7 +29,7 @@ export function Providers({ children }: { children: React.ReactNode }) {
   return (
     <WagmiProvider config={config}>
       <QueryClientProvider client={queryClient}>
-        <RainbowKitProvider initialChain={arcTestnet}>
+        <RainbowKitProvider theme={darkTheme()} initialChain={1227853239}>
           {children}
         </RainbowKitProvider>
       </QueryClientProvider>
